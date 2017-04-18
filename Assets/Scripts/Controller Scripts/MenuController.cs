16ebee453598948291501f6827d4c187729c3578
C5255900 <vitorluis.mauricio@hotmail.com>
2017-04-18 11:04:37 -0300

    MenuController and SceneFader
    Created MenuController and bird selection, created SceneFader

---
+++ Assets/Scripts/Controller Scripts/MenuController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	public static MenuController instance;
+
+	[SerializeField]
+	GameObject[] birds;
+	bool isGreenBirdUnlocked, isRedBirdUnlocked;
+
+	void Awake() {
+		CreateInstance ();
+	}
+
+	void Start() {
+		CheckUnlockedBirds ();
+
+		birds [GameController.instance.GetSelectedBird ()].SetActive (true);
+	}
+
+	void CreateInstance() {
+		if (instance == null)
+			instance = this;
+	}
+
+	void CheckUnlockedBirds() {
+		if (GameController.instance.IsGreenBirdUnlocked () == 1)
+			isGreenBirdUnlocked = true;
+		if (GameController.instance.IsRedBirdUnlocked () == 1)
+			isRedBirdUnlocked = true;
+	}
+
+	public void ChangeBird() {
+		if (GameController.instance.GetSelectedBird () == 0) {
+			if (isGreenBirdUnlocked) {
+				GameController.instance.SetSelectedBird (1);
+				birds [0].SetActive (false);
+				birds [GameController.instance.GetSelectedBird ()].SetActive (true);
+			}
+		} else if (GameController.instance.GetSelectedBird () == 1) {
+			if (isRedBirdUnlocked) {
+				GameController.instance.SetSelectedBird (2);
+				birds [1].SetActive (false);
+				birds [GameController.instance.GetSelectedBird ()].SetActive (true);
+			} else {
+				GameController.instance.SetSelectedBird (0);
+				birds [1].SetActive (false);
+				birds [GameController.instance.GetSelectedBird ()].SetActive (true);
+			}
+		} else if (GameController.instance.GetSelectedBird () == 2) {
+			GameController.instance.SetSelectedBird (0);
+			birds [2].SetActive (false);
+			birds [GameController.instance.GetSelectedBird ()].SetActive (true);
+		}
+	}
+
+	public void StartGame() {
+		SceneFader.instance.ChangeScene ("Gameplay");
+	}
+}
---
+++ Assets/Scripts/Fader Scripts/SceneFader.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFader : MonoBehaviour {
+
+	public static SceneFader instance;
+
+	[SerializeField]
+	GameObject canvas;
+	[SerializeField]
+	Animator animator;
+
+	void Awake() {
+		CreateSingleton ();
+	}
+
+	void CreateSingleton() {
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	public void ChangeScene(string scene) {
+		StartCoroutine (FadeInOut (scene));
+	}
+
+	IEnumerator FadeInOut(string scene) {
+		canvas.SetActive (true);
+		animator.Play ("FadeOut");
+		yield return new WaitForSeconds (1f);
+		SceneManager.LoadScene (scene);
+		animator.Play ("FadeIn");
+		yield return new WaitForSeconds (.7f);
+		canvas.SetActive (false);
+	}
+}
